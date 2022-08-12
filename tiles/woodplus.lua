f5735181ec34fa63ed7fda6eb48cacc977f1fdf3
Brian Robinson <sigma-144@hotmail.com>
2022-08-11 23:17:08 -0400

    Add files via upload

---
+++ tiles/woodplus.lua
@@ -0,0 +1,44 @@
+bannister_l = { type = Tile.Empty, frame = 1 }
+bannister_c = { type = Tile.Empty, frame = 2 }
+bannister_r = { type = Tile.Empty, frame = 3 }
+
+plank_top = { type = Tile.Solid, frame = 4 }
+plank_v = { type = Tile.Solid, frame = 8 }
+plank_bottom = { type = Tile.Solid, frame = 12 }
+
+plank_left = { type = Tile.Solid, frame = 13 }
+plank_h = { type = Tile.Solid, frame = 14 }
+plank_right = { type = Tile.Solid, frame = 15 }
+
+panel = { type = Tile.Solid, frame = 16 }
+
+carve_ul = { type = Tile.Solid, frame = 5 }
+carve_u = { type = Tile.Solid, frame = 6 }
+carve_ur = { type = Tile.Solid, frame = 7 }
+carve_l = { type = Tile.Solid, frame = 9 }
+carve_r = { type = Tile.Solid, frame = 11 }
+backing = { type = Tile.Solid, frame = 10 }
+
+ledge = { type = Tile.Ledge, frame = 17 }
+
+rafter_u = { type = Tile.Solid, frame = 18 }
+rafter_d = { type = Tile.Solid, frame = 19 }
+
+glitch = { type = Tile.Solid, frame = 20 }
+glitchhole = { type = Tile.Solid, frame = 21 }
+glitchledge = { type = Tile.Ledge, frame = 22 }
+
+buoy = { type = Tile.Solid, frame = 23 }
+
+cloud_l = { type = Tile.Empty, frame = 24 }
+cloud = { type = Tile.Sticky, frame = 25 }
+cloud_r = { type = Tile.Empty, frame = 26 }
+cloudledge = { type = Tile.Ledge, frame = 27 }
+
+water = { type = Tile.Water, frame = 28 }
+watersurface = { type = Tile.Water, frame = 29 }
+acid = { type = Tile.Acid, frame = 30 }
+acidsurface = { type = Tile.Acid, frame = 31 }
+
+
+
