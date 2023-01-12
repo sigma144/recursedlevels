9ce3d6ee5b6cfc3b5044cbe658c49ee5744ee501
Brian Robinson <sigma-144@hotmail.com>
2023-01-11 20:18:18 -0700

    Add files via upload

---
+++ tiles/castleplus.lua
@@ -0,0 +1,52 @@
+brick = { type = Tile.Solid, frame = 1 }
+
+brick_u = { type = Tile.Solid, frame = 2 }
+brick_d = { type = Tile.Solid, frame = 6 }
+brick_l = { type = Tile.Solid, frame = 4 }
+brick_r = { type = Tile.Solid, frame = 5 }
+
+wall = { type = Tile.Solid, frame = 3 }
+trim = { type = Tile.Solid, frame = 7 }
+
+chain_u = { type = Tile.Empty, frame = 8 }
+chain_d = { type = Tile.Empty, frame = 12 }
+
+pillar_u = { type = Tile.Solid, frame = 10 }
+pillar_d = { type = Tile.Solid, frame = 14 }
+
+bars = { type = Tile.Solid, frame = 16 }
+
+water = { type = Tile.Water, frame = 20 }
+watersurface = { type = Tile.Water, frame = 21 }
+camera_l = { type = Tile.Empty, frame = 22 }
+camera_r = { type = Tile.Empty, frame = 23 }
+
+waterchain_u = { type = Tile.Water, frame = 9 }
+waterchain_d = { type = Tile.Water, frame = 13 }
+
+waterpillar_u = { type = Tile.Solid, frame = 11 }
+waterpillar_d = { type = Tile.Solid, frame = 15 }
+
+waterbars = { type = Tile.Solid, frame = 17 }
+
+buoyul = { type = Tile.Solid, frame = 24 }
+buoyu = { type = Tile.Solid, frame = 25 }
+buoyur = { type = Tile.Solid, frame = 26 }
+buoyl = { type = Tile.Solid, frame = 28 }
+buoyc = { type = Tile.Solid, frame = 29 }
+buoyr = { type = Tile.Solid, frame = 30 }
+buoydl = { type = Tile.Solid, frame = 32 }
+buoyd = { type = Tile.Solid, frame = 33 }
+buoydr = { type = Tile.Solid, frame = 34 }
+
+glitch = { type = Tile.Solid, frame = 27 }
+glitchl = { type = Tile.Empty, frame = 31 }
+glitchr = { type = Tile.Empty, frame = 35 }
+glitchledge = { type = Tile.Ledge, frame = 18 }
+glitchbars = { type = Tile.Solid, frame = 19 }
+
+cloud_l = { type = Tile.Empty, frame = 36 }
+cloud = { type = Tile.Sticky, frame = 37 }
+cloud_r = { type = Tile.Empty, frame = 38 }
+cloudledge = { type = Tile.Ledge, frame = 39 }
+
