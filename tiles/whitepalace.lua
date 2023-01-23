82821220a454f18e95bd13868317cc13d37def31
Brian Robinson <sigma-144@hotmail.com>
2023-01-22 19:21:39 -0700

    Add files via upload

---
+++ tiles/whitepalace.lua
@@ -0,0 +1,84 @@
+buoy_ul = {type=Tile.Solid, frame=1}
+buoy_u = {type=Tile.Solid, frame=2}
+buoy_ur = {type=Tile.Solid, frame=3}
+buoy_l = {type=Tile.Solid, frame=21}
+buoy_c = {type=Tile.Solid, frame=22}
+buoy_r = {type=Tile.Solid, frame=23}
+buoy_dl = {type=Tile.Solid, frame=41}
+buoy_d = {type=Tile.Solid, frame=42}
+buoy_dr = {type=Tile.Solid, frame=43}
+buoy_v_u = {type=Tile.Solid, frame=20}
+buoy_v_c = {type=Tile.Solid, frame=40}
+buoy_v_d = {type=Tile.Solid, frame=60}
+buoy_h_l = {type=Tile.Solid, frame=61}
+buoy_h_c = {type=Tile.Solid, frame=62}
+buoy_h_r = {type=Tile.Solid, frame=63}
+
+brick_ul = {type=Tile.Solid, frame=4}
+brick_ul_w = {type=Tile.Solid, frame=24}
+brick_ul_a = {type=Tile.Solid, frame=44}
+brick_ur = {type=Tile.Solid, frame=8}
+brick_ur_w = {type=Tile.Solid, frame=28}
+brick_ur_a = {type=Tile.Solid, frame=48}
+brick_l = {type=Tile.Solid, frame=5}
+brick_m = {type=Tile.Solid, frame=6}
+brick_r = {type=Tile.Solid, frame=7}
+brick_dl = {type=Tile.Solid, frame=25}
+brick_dm = {type=Tile.Solid, frame=26}
+brick_dr = {type=Tile.Solid, frame=27}
+line_v = {type=Tile.Solid, frame=45}
+line_h = {type=Tile.Solid, frame=46}
+brick_d = {type=Tile.Solid, frame=47}
+block = {type=Tile.Solid, frame=64}
+
+pillarv_u = {type=Tile.Solid, frame=50}
+pillarv_c = {type=Tile.Solid, frame=51}
+pillarv_d = {type=Tile.Solid, frame=52}
+
+pillar_ull = {type=Tile.Solid, frame=9}
+pillar_ull_w = {type=Tile.Solid, frame=29}
+pillar_ull_a = {type=Tile.Solid, frame=49}
+pillar_ul = {type=Tile.Solid, frame=10}
+pillar_u = {type=Tile.Solid, frame=11}
+pillar_ur = {type=Tile.Solid, frame=12}
+pillar_urr = {type=Tile.Solid, frame=13}
+pillar_urr_w = {type=Tile.Solid, frame=33}
+pillar_urr_a = {type=Tile.Solid, frame=53}
+pillar_l = {type=Tile.Solid, frame=30}
+pillar_m = {type=Tile.Solid, frame=31}
+pillar_r = {type=Tile.Solid, frame=32}
+
+ledge = {type=Tile.Ledge, frame=14}
+ledge_w = {type=Tile.Solid, frame=34}
+ledge_a = {type=Tile.Solid, frame=54}
+ledge_x = {type=Tile.Ledge, frame=15}
+ledge_x_w = {type=Tile.Solid, frame=35}
+ledge_x_a = {type=Tile.Solid, frame=55}
+water_surface = {type=Tile.Water, frame=36}
+acid_surface = {type=Tile.Acid, frame=56}
+water = {type=Tile.Water, frame=37}
+acid = {type=Tile.Acid, frame=57}
+
+black_l = {type=Tile.Empty, frame=16}
+black = {type=Tile.Solid, frame=17}
+black_hole = {type=Tile.Solid, frame=18}
+black_r = {type=Tile.Empty, frame=19}
+
+cloud_l = { type = Tile.Empty, frame = 38 }
+cloud = { type = Tile.Sticky, frame = 58 }
+cloud_r = { type = Tile.Empty, frame = 39 }
+cloudledge = { type = Tile.Ledge, frame = 59 }
+
+ruin1 = {type=Tile.Solid, frame=65}
+ruin2 = {type=Tile.Solid, frame=66}
+grass1 = {type=Tile.Solid, frame=67}
+grass2 = {type=Tile.Solid, frame=68}
+
+glitch_tunnel = { type = Tile.Solid, frame = {69, 70, 71}, time = 0.14 }
+glitch_check = { type = Tile.Solid, frame = {72, 73}, time = 0.12 }
+glitch_static1 = { type = Tile.Solid, frame = { 74, 75, 76, 77, 78}, time = 0.07 }
+glitch_static2 = { type = Tile.Solid, frame = { 75, 74, 77, 78, 76}, time = 0.07 }
+glitch_static3 = { type = Tile.Solid, frame = { 76, 77, 78, 74, 75}, time = 0.07 }
+glitch_static4 = { type = Tile.Solid, frame = { 78, 76, 74, 75, 77}, time = 0.07 }
+glitch_static5 = { type = Tile.Solid, frame = { 77, 78, 75, 76, 74}, time = 0.07 }
+glitch_ugly = { type = Tile.Solid, frame = 79 }
